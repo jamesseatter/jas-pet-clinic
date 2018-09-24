22ccd1d7e52c76365d043bc32dcf008a450929c7
james seatter <james.seatter@hotmail.co.uk>
2018-09-24 10:57:22 +0200

    Initial Project Add

---
+++ src/main/java/eu/seatter/jaspetclinic/JasPetClinicApplication.java
@@ -0,0 +1,12 @@
+package eu.seatter.jaspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/eu/seatter/jaspetclinic/JasPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.seatter.jaspetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JasPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
