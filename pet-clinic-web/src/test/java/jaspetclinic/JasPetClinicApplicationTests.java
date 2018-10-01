842e8b9a0da1ac97ba5075c72aede82a921f3e72
james seatter <james.seatter@hotmail.co.uk>
2018-10-01 16:46:48 +0200

    Closed #1 "Create Multi-Module Project for Data Model" https://github.com/jamesseatter/jas-pet-clinic/issues/1#issue-363061753

--- pet-clinic-web/src/test/java/jaspetclinic/JasPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/jaspetclinic/JasPetClinicApplicationTests.java
@@ -1,12 +1,13 @@
-package eu.seatter.jaspetclinic;
+package jaspetclinic;
 
+import eu.seatter.jaspetclinic.JasPetClinicApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = JasPetClinicApplication.class)
 public class JasPetClinicApplicationTests {
 
     @Test
