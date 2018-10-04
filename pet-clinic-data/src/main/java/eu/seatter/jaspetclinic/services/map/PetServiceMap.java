5e4c26b8371af39aea0cea5a5795d9d98b0889d0
james seatter <james.seatter@hotmail.co.uk>
2018-10-04 11:10:17 +0200

    Implement Map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package eu.seatter.jaspetclinic.services.map;
+
+import eu.seatter.jaspetclinic.model.Pet;
+import eu.seatter.jaspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
