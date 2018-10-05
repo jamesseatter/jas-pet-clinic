db4a40526dea05dc0c5705657d95c95ae3b032c4
james seatter <james.seatter@hotmail.co.uk>
2018-10-05 20:04:08 +0200

    Added Vets index page and controller. Closed #17

---
+++ pet-clinic-web/src/main/java/eu/seatter/jaspetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package eu.seatter.jaspetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping("/vets")
+    public String listView() {
+        return "vets/index";
+    }
+}
