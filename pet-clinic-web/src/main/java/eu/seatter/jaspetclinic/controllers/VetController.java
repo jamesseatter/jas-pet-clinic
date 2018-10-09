2d0a6bad9bcf6b43f2218c503906e4f1ee458289
james seatter <james.seatter@hotmail.co.uk>
2018-10-09 14:02:56 +0200

    Implement List all vets on vets index page. Closed #17

--- pet-clinic-web/src/main/java/eu/seatter/jaspetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/eu/seatter/jaspetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package eu.seatter.jaspetclinic.controllers;
 
+import eu.seatter.jaspetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String listView() {
+    public String listView(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
