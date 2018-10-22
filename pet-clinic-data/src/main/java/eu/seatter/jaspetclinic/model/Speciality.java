fabb5015503825de210f7903d17df7ce6f49d53d
james seatter <james.seatter@hotmail.co.uk>
2018-10-22 15:59:14 +0200

    Implement Create Vet Speciality, associate to Vet. Closed #29

---
+++ pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package eu.seatter.jaspetclinic.model;
+
+/**
+ * Created by jas on 22/10/2018
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
