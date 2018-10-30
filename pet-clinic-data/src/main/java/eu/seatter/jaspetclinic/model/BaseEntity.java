9c7f7a6fb96d4ba6b8387c1509090524741c80e2
james seatter <james.seatter@hotmail.co.uk>
2018-10-30 16:20:40 +0100

    Implement Implement the base entity (JPA). Closed #37

--- pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/BaseEntity.java
@@ -1,9 +1,15 @@
 package eu.seatter.jaspetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    //hibernate recommends boxed types instead of base types.
 
     public Long getId() {
