8b638714afbaacb518af6bec3f99a1584ae41119
james seatter <james.seatter@hotmail.co.uk>
2018-10-30 16:36:43 +0100

    Implement Convert Vets to JPA entities. Closed #39

--- pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package eu.seatter.jaspetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by jas on 22/10/2018
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name="description")
     private String description;
 
     public String getDescription() {
