b287e2689192d5be7267ce73f1cd53c6ed0f0f52
james seatter <james.seatter@hotmail.co.uk>
2018-10-30 16:31:20 +0100

    Implement Convert Owners to JPA entities. Closed #38

--- pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package eu.seatter.jaspetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name="name")
     private String name;
 
     public String getName() {
