f39821b9c3b0a7c9000eb450d8d0f55650f2e453
james seatter <james.seatter@hotmail.co.uk>
2018-10-31 14:03:44 +0100

    Implement Create Visits entity. Closed #40

--- pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package eu.seatter.jaspetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by jas on 22/10/2018
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name="pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
