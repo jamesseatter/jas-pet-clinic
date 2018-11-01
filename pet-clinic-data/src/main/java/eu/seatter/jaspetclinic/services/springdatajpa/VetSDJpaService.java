b9b6a2ec7e8229a0c32c641b591633be16ac4003
james seatter <james.seatter@hotmail.co.uk>
2018-11-01 09:29:58 +0100

    Implement Create Spring Data JPA Entity for Vets. Closed #44 Implement Create Spring Data JPA Service for Pet Types. Closed #45

---
+++ pet-clinic-data/src/main/java/eu/seatter/jaspetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,54 @@
+package eu.seatter.jaspetclinic.services.springdatajpa;
+
+import eu.seatter.jaspetclinic.model.Vet;
+import eu.seatter.jaspetclinic.repositories.VetRepository;
+import eu.seatter.jaspetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jas
+ * Date: 01/11/2018
+ * Time: 09:23
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
